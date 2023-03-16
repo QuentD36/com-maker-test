19c52e7ddf400ed8c4f1399a9b336c485c6a769f
QuentD36 <82884437+QuentD36@users.noreply.github.com>
2023-03-16 21:34:33 +0100

    first commit

---
+++ src/components/Datagrid.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'name',
+    headerName: 'Name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'username',
+    headerName: 'Username',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'email',
+    headerName: 'Email',
+    width: 110,
+  },
+  {
+    field: 'website',
+    headerName: 'Website',
+    width: 160,
+  },
+];
+
+
+export default function CustomDataGrid() {
+
+    const data = []
+    
+    return (
+        <Box sx={{ width: '100%' }}>
+        <DataGrid
+            autoHeight
+            rows={data}
+            columns={columns}
+            initialState={{
+            pagination: {
+                paginationModel: {
+                pageSize: 5,
+                },
+            },
+            }}
+            pageSizeOptions={[5]}
+            checkboxSelection
+        />
+        </Box>
+    );
+}
